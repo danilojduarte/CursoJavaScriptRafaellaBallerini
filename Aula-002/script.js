f8ee5baefafd26f469ce5e84e1107d535c88a2a6
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-01-30 15:08:22 -0300

    Iniciando lista

--- Aula-002/script.js
+++ Aula-002/script.js
@@ -1,6 +1,13 @@
 function adicionarTarefa() {
-  let mensagem = "Tarefa adicionada com sucesso!";
-        document.getElementById("mensagem").textContent = mensagem;
+  //let mensagem = "Tarefa adicionada com sucesso!";
+        
+
+  let inputTarefa = document.getElementById("inputTarefa");
+  let tarefa = inputTarefa.value;
+  document.getElementById("mensagem").textContent = tarefa;
+
+
+  inputTarefa.value = "";
 }
 
 // Criando function para add lista de tarefas;
